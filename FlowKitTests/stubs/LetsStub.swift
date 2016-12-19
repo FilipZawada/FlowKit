0cd2af0f2a548812fc3cf2bc504613220a6ed992
Filip Zawada <me@filimanjaro.com>
2016-12-19 10:57:06 +0100

    test: remove stub's unused methods

--- FlowKitTests/stubs/LetsStub.swift
+++ FlowKitTests/stubs/LetsStub.swift
@@ -6,34 +6,4 @@
 import FlowKit
 import UIKit
 
-class LetsStub: Lets {
-
-    func push<Args, ViewController>(_ flow: Flow<ViewController>,
-                                    animated: Bool = true,
-                                    with getConfigure: ((ViewController) -> ((Args) -> Void))? = nil)
-                    -> (Args) -> Void where ViewController: UIViewController {
-
-        return { _ in }
-    }
-
-    func pop(animated: Bool = true)  -> () -> Void {
-        return {}
-    }
-
-    func popTo<U>(_ flow: Flow<U>, animated: Bool = true) -> () -> Void {
-        return {}
-    }
-
-    func popToRoot(animated: Bool = true) -> () -> Void {
-        return {}
-    }
-
-    func present<U>(_ flow: Flow<U>, animated: Bool = true) -> () -> Void {
-        return {}
-    }
-
-    func dismiss(animated: Bool = true) -> () -> Void {
-        return {}
-    }
-
-}
+class LetsStub: Lets {}
